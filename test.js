a78614ad0ef8dd65f45b771f28468ca51c45a1e0
joshbuddy <joshbuddy@gmail.com>
2020-08-14 21:03:53 -0400

    Bump version to 1.0.1

--- test.js
+++
@@ -1,9 +0,0 @@
-const toStream = require('buffer-to-stream')
-const fs = require('fs')
-const ExifTransformer = require('./index')
-const img = fs.readFileSync('./Canon_40D.jpg')
-console.log(img)
-
-const writer = fs.createWriteStream('out.jpg')
-
-toStream(img).pipe(new ExifTransformer()).pipe(writer)

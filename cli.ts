bb24688b9284eb6302b8cd26f3415891c4ca342f
Josh Hull <josh@fireflop.com>
2020-08-16 14:34:11 -0400

    fix lint

--- cli.ts
+++ cli.ts
@@ -9,5 +9,5 @@ const reader = createReadStream(args[0])
 const writer = createWriteStream(args[1])
 
 reader.pipe(new ExifBeGone()).pipe(writer).on('finish', () => {
-	console.log(`Finishing removing exif data from ${args[0]}, written to ${args[1]}`)
+  console.log(`Finishing removing exif data from ${args[0]}, written to ${args[1]}`)
 })
